1985b8d7efc705f58832dca6c8c0bc8adc33d543
Hack Reactor Students <github@hackreactor.com>
2013-06-01 17:31:45 -0700

    wrote string iterator _each and created file 'chatbuilder.js'

---
+++ chatbuilder.js
@@ -0,0 +1,12 @@
+// string iterator to pass as argument to Chat.fetch
+var _each = function(array) {
+for (var i = 0; i < array.length; i++) {
+  console.log(array[i]);}
+};
+
+// Method to display chat to Client
+
+
+// Method to fetch chat from the server
+
+
