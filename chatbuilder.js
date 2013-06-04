f22572ed9af88d72780ae5af21cf3e9b40687846
dklounge <daviddarden11@gmail.com>
2013-06-04 13:06:47 -0700

    fixed Chat.fetch and setInterval documentation

--- chatbuilder.js
+++ chatbuilder.js
@@ -1,8 +1,11 @@
 // iterator to pass as argument to Chat.fetch
 var _each = function(array) {
-for (var i = 0; i < array.length; i++) {
-  Chat.display(array[array.length-1]);}
+//for (var i = 0; i < array.length; i++) {
+  // removed for loop because you only need to display one line
+  Chat.display(array[array.length-1]);
 };
+
+// Implementation of setInterval - use quotes - setInterval("function()", 3000);
 
 // Perform an asynchronous http request using jQuery
 // var getValue = $.ajax(Chat.fetch(_each));
